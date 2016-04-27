54dc4d0ef979608d684921ad53830a901b17c68e
YsChiao <yis.qiao@gmail.com>
2016-04-28 00:33:39 +0200

    1.TwoSum

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,5 +1,5 @@
 /*
- * Two Sum
+ * Two Sum, Problem No.1
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution.
